3a7e571f8cd0c857d6fea82717a7f319e8cf4dd0
HighOnMelatonin <tanruianh@outlook.com>
2023-09-04 20:34:14 +0800

    using a dict to track repeated values

--- python/graduation.py
+++ python/graduation.py
@@ -1,9 +1,9 @@
 # Graduation
 '''
-Number of unique classes in a column
+Create a stack for every column, as the cursor parses through the column, add every unique letter to the stack
+
+If a letter in the column 
 '''
-
-
 line = input()
 n, m, k = line.split(' ')
 
@@ -15,23 +15,27 @@ for i in range(int(n)):
 hats = [[]]
 j = 0
 letters = []    # letters that already have a hat associated
-letterDict = [] # shows the index of the letter in "hats"
+letterDict = {} # shows the index of the letter in "hats"
 for i in range(int(m)):
     exists = False
-    # checks if there are repeated letters in the column (i.e. 'A' appeared twice in the column)
+    column = [] #Column contains all the unique letters in the column
     for line in lines:
-        if line[i] in hats[j]:
+        if line[i] in letterDict:
             exists = True
+            j = letterDict[line[i]]
 
-        else:
+        elif not line[i] in column:
+            column.append(line[i])
 
-            if exists:
-                hats[j] += [line[i]]
+    if exists:
+        hats[j] += column
 
-            else:
-                hats.append([line[i]])
-                j += 1
-                exists = True
+    else:
+        j += 1
+        for item in column:
+            letterDict.update({item: j})
+        hats.append(column)
+
 
 print(hats)
 print(len(hats) - 1)
